ea1f7206839d2bcc95fe08f9ef09d60a8afc0dc2
Hibernon Eric <hibernoneric@gmail.com>
2025-10-05 20:27:45 -0300

    chore: primeiro commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Peça ao usuário sua lista de compras e imprima-a
+lista_de_compras = input("Insira sua lista de compras separada por vírgulas:  ").split(",")
+print(lista_de_compras)
+
+# Peça ao usuário o índice do item que deseja remover 
+indice_para_remover =  int(input("Insira o índice do item a aser removido (lembre-se de começar a contar a partir de 0):  "))
+
+# Remova o item  desejado e imprima-o
+item_removido = lista_de_compras.pop(indice_para_remover)
+print("Removido : " + item_removido)
+
+# Imprima a lista de compras atualizada
+print("Sua lista de compras atualizada:  " + ','.join(lista_de_compras))
